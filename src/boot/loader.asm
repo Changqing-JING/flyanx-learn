32ea0b9a588b8ce1cd9edf92b98069228e3a8f59
Changqing <changqing.jing@outlook.com>
2020-04-01 21:24:50 +0800

    add mem check

--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -1,4 +1,4 @@
-[bits 16]
+
 
 org 0x100
 StackBase equ 0x100
@@ -7,6 +7,7 @@ jmp start
 
 start:
     mov ax, cs
+    mov es, ax
     mov ds, ax
     mov ss, ax
     mov sp, StackBase
@@ -14,11 +15,62 @@ start:
     mov bp, Message
     call DispStr
 
-    jmp $
+    xor ebx, ebx ;start with 0
+    mov di, _MemChkBUf
+;save ARDS in es:di
+MemChkLoop:
+    mov eax, 0x0000e820
+    mov ecx, 20
+    mov edx, 0x0534d4150
+    int 0x15
+    jc MemChkFail  ;CF == 1
+    ; CF == 0
+    add di, 20
+    inc dword [_ddMCRCount]
 
+    cmp ebx, 0
+    je MemChkFinish;ebx==0 mean last one
+    jmp MemChkLoop
+
+MemChkFail:
+    mov dword [_ddMCRCount], 0
+    mov bp, MessageMemChkFailed
+    call DispStr
+    jmp $
+MemChkFinish:
+    mov bp, MessageMemChkSuccess
+    call DispStr
+    jmp $
 
 
 
 %include "DispStr.inc"
 
 Message: db "Hello Loader!"
+MessageMemChkSuccess db "Mem Chk OK   "
+MessageMemChkFailed db "Mem Chk Fail "
+
+;16bits data
+
+_ddMCRCount dd 0 ;memory check result , how many ADRS is got
+_ddMemSize dd 0
+
+;Address Range Descriptor Struct
+_ADRS:
+    _addbaseAddLow dd 0
+    _addbaseAddHigh dd 0
+    _addbaseLengthLow dd 0
+    _addbaseLengthHeigh dd 0
+    _ddType dd 0
+
+;256/20 = 12.8 can contain 12 ARDS
+_MemChkBUf: times 256 db 0
+
+;32bit data
+[section data32]
+align 32
+data32:
+
+
+
+
